a4463e3f2ba90d6d8e43a0afeeea50c50c4fe8f6
machdd <jkk1637@naver.com>
2023-05-24 22:29:28 +0900

    no message

---
+++ src/main/java/com/machdd/EcoAndRich_BackEnd_test_20230522/EcoAndRichBackEndTest20230522Application.java
@@ -0,0 +1,13 @@
+package com.machdd.EcoAndRich_BackEnd_test_20230522;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoAndRichBackEndTest20230522Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoAndRichBackEndTest20230522Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/machdd/EcoAndRich_BackEnd_test_20230522/EcoAndRichBackEndTest20230522ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machdd.EcoAndRich_BackEnd_test_20230522;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoAndRichBackEndTest20230522ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
